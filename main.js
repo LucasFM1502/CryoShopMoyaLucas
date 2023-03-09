0db9f168d1622a622f318c05c75b854ddc2d3e90
LucasFM <lucas.moya@hotmail.com>
2023-03-08 22:11:09 -0300

    Segunda Entrega de JS

--- main.js
+++ main.js
@@ -1,37 +1,77 @@
 
+class Juego {
+  constructor(nombre, precio){
+          this.nombre = nombre;
+          this.precio = precio;
+}
+}
+
+function TotalPrevio(array){
+      let total = 0;
+      array.forEach ( JuegoCompra => {
+        total = JuegoCompra.precio * JuegoCompra.cantidad
+      })
+        return total;
+}
+
 function Impuestos(precioArg) {
     const IVA = 1.21
     const servDigital = 0.05
     const impPais = 0.08
     const percepcion = 0.45
     
+  
     return (IVA + servDigital + impPais + percepcion) * precioArg
 }
 
 function Billetera(precioFinal) {
-      if (precioFinal > 2000) {
+      if (precioFinal > 6000) {
         return "Ouch! Todo sea por vicear."
       }else{
         return "¡Bien ahi con los ahorros!"
       }
 }
 
-let nombre = prompt("Bienvenido al calculo de impuestos para juegos en Argentina. Ingrese su nombre y apellido.")
+const carritoCompra = [ ]
+const listadeJuegos = [ { ID: 1, nombre: "TLOU", precio: 2500, cantidad: 100},
+                        { ID: 2, nombre: "L4D2", precio: 800, cantidad: 100},
+                        { ID: 3, nombre: "Skyrim", precio: 1200, cantidad: 100}]
+
+
+listadeJuegos.forEach (el => {
+    console.log (el.ID)
+    console.log (el.nombre)
+    console.log (el.precio)
+
+})
+
+
+let nombre = prompt("Bienvenidx a la tienda de juegos digitales para Argentina. Ingrese su nombre y apellido.")
 alert ("Bienvenidx " +nombre)
 
 let mensajeFinal = ""
 
-while (mensajeFinal != "ESC") {
+do {
 
-let juego = prompt("Ingrese el juego deseado.")
-alert ("El juego elegido es " +juego)
+  let id = prompt("Ingrese el ID del juego deseado.")
+  if (!isNaN(id)) {
+        if (listadeJuegos.some( juego => juego.ID == id)) {
 
-let precioArg = Number(prompt("Indique el valor del juego."))
-alert ("Su juego previo a impuestos es de $" +precioArg)
+          let cantidad = prompt("¿Cuantos desea llevar?")
+          const JuegoCompra = (listadeJuegos.find( juego => juego.ID == id))
+          JuegoCompra.cantidad = cantidad;
+          carritoCompra.push(JuegoCompra)
+        }else{ alert ("ID no valido.")}
+  }
+
+
+  mensajeFinal = prompt("Si ya ha finalizado con su compra ingrese \"SI\". De lo contrario ingrese cualquier caracter para continuar.").toUpperCase()
+}while (mensajeFinal != "SI")
+
+let precioenArg = alert("Su juego previo a impuestos es de $" + TotalPrevio(carritoCompra))
+let precioArg = TotalPrevio(carritoCompra)
+
 
 precioFinal = Impuestos(precioArg)
-alert ("El valor del juego con impuestos agregados es de $" +precioFinal)
-alert (Billetera(precioFinal))
-
-mensajeFinal = prompt("Si ya ha finalizado con el simulador ingrese \"ESC\". De lo contrario ingrese cualquier caracter para volver a comenzar.").toUpperCase()
-}
+alert("El valor del juego con impuestos agregados es de $" + precioFinal + "\nGracias por su compra!")
+alert(Billetera(precioFinal))
